d5fc6254d93f7c2a845a10338a38b44ad49c2644
Taylor Marlow <metallicax4xyou@gmail.com>
2025-03-30 03:23:37 +0000

    update

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ last_milestone_check_value = timer # To prevent multiple triggers for same miles
 # Access secrets from environment variables
 try:
     bot_token = os.environ['BOT_TOKEN']
-        channel_id_str = os.environ['CHANNEL_ID']
+        channel_id_str = os.environ['1354327441673293886']
             TARGET_CHANNEL_ID = int(channel_id_str)
             except KeyError:
                 print("ERROR: BOT_TOKEN or CHANNEL_ID not found in Repl.it Secrets.")
